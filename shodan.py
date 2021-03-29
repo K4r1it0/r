b0d17df97e718190f9349464eca7eee105d30bb6
K4r1it0 <39193373+K4r1it0@users.noreply.github.com>
2021-03-29 23:47:37 +0200

    Update shodan.py

--- shodan.py
+++ shodan.py
@@ -1,32 +1,79 @@
 import requests
+import urllib3
 from bs4 import BeautifulSoup
+import signal
 import re
 import time
-url =""
-ips=set()
+urllib3.disable_warnings()
+username=""
+password=""
+query = input("Query: ")#"ssl:oktaprise"
+
+
+def session(username,password):
+	global headers
+	headers = {
+	    'Content-Type': 'application/x-www-form-urlencoded',
+	    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
+	    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'}
+
+	session = requests.session()
+	csrfres = session.get("https://account.shodan.io/login",headers=headers).text
+	csrf = BeautifulSoup(csrfres,'html.parser').find('input',{"name":"csrf_token"})['value']
+	data = 'username={}&password={}&grant_type=password&continue=https%3A%2F%2Faccount.shodan.io%2F&csrf_token={}'.format(username,password,csrf)
+	r = session.post('https://account.shodan.io/login', headers=headers, data=data)
+	return session
+
+
+def getlen(session):
+
+	response = session.get("https://www.shodan.io/search/_summary?query={}".format(query),headers=headers).text
+	length = BeautifulSoup(response,'html.parser').find('div',{"class":"bignumber"}).text
+	return int(length)-1
+
+
+def getd(query,url,session):
+	paramsPost = {"query":"{} {}".format(query,url)}
+	response = session.post("https://www.shodan.io/search", data=paramsPost, headers=headers)
+	return response.text
+
+
+session = session()
+length = getlen(session(u,p))
+reip = 0
 c=0
-def getd(url):
-	session = requests.Session()
-	paramsPost = {"query":"http.favicon.hash:784872924 {}".format(url)}
-	headers = {"Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9","Connection":"close","User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36","Referer":"https://www.shodan.io/search?query=http.favicon.hash%3A784872924+-ip%3A77.68.82.43+-ip%3A142.93.118.223+-ip%3A162.208.0.242+-ip%3A70.182.190.49+-ip%3A52.33.195.99+-ip%3A91.250.103.181+-BLKBOX","Sec-Fetch-Site":"same-origin","Sec-Fetch-Dest":"document","Accept-Encoding":"gzip, deflate","Sec-Fetch-Mode":"navigate","Cache-Control":"max-age=0","Upgrade-Insecure-Requests":"1","Sec-Fetch-User":"?1","Accept-Language":"en-US,en;q=0.9,ar;q=0.8","Content-Type":"application/x-www-form-urlencoded"}
-	cookies = {"_LOCALE_":"en","_ga":"GA1.2.836267779.1610769571","polito":"\"<>!\"","__cfduid":"<>","session":"<>="}
-	response = session.post("https://www.shodan.io/search", data=paramsPost, headers=headers, cookies=cookies)
-	return response
+ips=[]
+url =""
+counter = len(ips) 
 
+for result in range(length):
+	if reip < 3:
+		res = getd(query,url,session)
 
-while c < 100:
-	res = getd(url).text
-	if "Invalid search query" in res:
-		print("Results")
-		for i in ips:
-			print(i)
-		break
+		if "Invalid search query" in res:
+			print("Invalid search query")
+			break
+
+		if "Daily search usage limit reached" in res:
+			print("Daily search usage limit reached")
+			for i in list(dict.fromkeys(ips)):
+				print(i)
+			break
+		elif len(ips) == length:
+			for i in list(dict.fromkeys(ips)):
+				print(i)
+			break
+		else:
+			s= re.findall(r'/host/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}',res)	
+			for i in s:
+				if not i.replace('/host/',"") in ips:
+					url +=" -ip:{}".format(i.replace('/host/',""))
+					ips.append(i.replace('/host/',""))
+			c+=1
+			if len(ips) == counter:
+				reip +=1
+				time.sleep(10)
+			counter = len(ips)	
 	else:
-		s= re.findall(r'/host/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}',res)
-		for i in s:
-			if not i.replace('/host/',"") in ips:
-				url +=" -ip:{}".format(i.replace('/host/',""))
-				ips.add(i.replace('/host/',""))
-		c+=1
-		print(len(ips),"Total IPS Length")
-		time.sleep(10)
+		for i in list(dict.fromkeys(ips)):
+					print(i)
